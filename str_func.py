b4f333ea5492837910b79e7a09656369ae9bf616
AlexeyYanchenkov <alexeyyanchenkov25@gmail.com>
2024-03-08 13:11:19 +0300

    New func in programm

--- str_func.py
+++ str_func.py
@@ -2,3 +2,9 @@ def upper_line():
     """Делает буквы заглавными """
     string = input()
     return string.upper()
+
+
+def capitalize_line():
+    """Делает первую букву заглавной"""
+    string_line = input()
+    return string_line.capitalize()
